cbb0ff727c5503628cc50cc020388941e924f19c
sliceoflif3 <yunomiclover@gmail.com>
2025-03-13 01:56:05 +0700

    back-end init

---
+++ back-end/toodoo/src/main/java/com/example/toodoo/ToodooAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.toodoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToodooAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToodooAppApplication.class, args);
+	}
+
+}
